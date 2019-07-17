eaba7725802d46e2ea44e8d3ca92b31f0c5076ae
Jonathan Wieben <joni.wieben@icloud.com>
2019-07-17 21:51:54 +0200

    New: Make it look cool

---
+++ pages/_app.js
@@ -0,0 +1,50 @@
+import App, { Container } from 'next/app'
+import Head from 'next/head'
+import React from 'react'
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Head>
+          <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="description" content="The last URL shortener you will ever need." />
+          <meta name="keywords" content="btfl url shortener" />
+          <meta name="author" content="Jonathan Wieben" />
+          <title>btfl.link</title>
+          <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500" rel="stylesheet" />
+        </Head>
+        <Component {...pageProps} />
+        <style global jsx>{`
+          html {
+            font-family: 'Montserrat', sans-serif;
+            font-size: 16px;
+            word-spacing: 1px;
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+            -moz-osx-font-smoothing: grayscale;
+            -webkit-font-smoothing: antialiased;
+            box-sizing: border-box;
+          }
+
+          *,
+          *:before,
+          *:after {
+            box-sizing: border-box;
+            margin: 0;
+            outline: none;
+          }
+
+          body {
+            background-color: #f26e03;
+          }
+        `}</style>
+      </Container>
+    )
+  }
+}
+
+export default MyApp
