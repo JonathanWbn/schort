9866b081ef670408328bae12f6aa9590fce4cc46
Jonathan Wieben <joni.wieben@icloud.com>
2019-07-30 13:00:59 +0200

    Chore: Add cypress and some basic tests

---
+++ cypress/integration/create-link.spec.js
@@ -0,0 +1,26 @@
+describe('creating link', function() {
+  it('fills form and successfully submits', function() {
+    cy.server()
+    cy.route({
+      method: 'POST',
+      url: '/redirect',
+      response: {
+        success: true,
+      },
+    })
+
+    cy.visit('/')
+
+    cy.get('input')
+      .first()
+      .type('https://www.google.com')
+    cy.get('input')
+      .last()
+      .type('google')
+
+    cy.get('button').click()
+
+    cy.contains('Successfully created link. Click me to copy.').click()
+    cy.contains('Copied.')
+  })
+})
